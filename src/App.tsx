aead725fbd3832b6045ff3e2befb9b61b53a7bfb
scoobytux <tu.lna07@gmail.com>
2022-09-09 21:59:13 +0700

    feat: fetch pokemons

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+
+import axios from "axios";
 
 // Style
 import "./App.css";
+import { useEffect } from "react";
+
+interface Pokemon {
+  id?: number;
+  url?: string;
+  name: string;
+  sprites?: {
+    front_default: string;
+  };
+}
 
 const App: React.FC = () => {
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    const getPokemon = async () => {
+      const res = await axios(
+        "https://pokeapi.co/api/v2/pokemon?limit=20&offset=20"
+      );
+
+      res.data.results.forEach(async (pokemon: Pokemon) => {
+        const poke = await axios(
+          `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
+        );
+        setPokemons([...pokemons, poke.data]);
+      });
+    };
+  });
+
   return (
     <div className="App">
       <div className="container">
