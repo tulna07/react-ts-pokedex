89422f31eae8efc0248a135392b05a5d573580c0
scoobytux <tu.lna07@gmail.com>
2022-09-09 20:57:48 +0700

    chore: clean up files

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import React from "react";
-import HelloWorld from "components/HelloWorld";
 
-const App: React.FC = () => <HelloWorld />;
+// Style
+import "./App.css";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="pokemon-header">Pokemon</header>
+      </div>
+    </div>
+  );
+};
 
 export default App;
